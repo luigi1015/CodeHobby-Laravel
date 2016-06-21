976a64ee090c77be74aaa352998740e8240c89a9
Jeff <github@314ghz.com>
2016-06-21 17:19:42 -0500

    Added a task schedule.

--- CodeHobby/app/Console/Kernel.php
+++ CodeHobby/app/Console/Kernel.php
@@ -7,24 +7,23 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
 		Commands\UpdateProjects::class,
-    ];
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	 /**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		$schedule->command('updateprojects')->daily()->withoutOverlapping();
+	}
 }
