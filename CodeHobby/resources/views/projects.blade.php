7d31b6c538e6e16bbeaa988d338022b1ccb09595
Jeff <github@314ghz.com>
2016-07-02 12:02:44 -0500

    Added some text to the Projects page.

--- CodeHobby/resources/views/projects.blade.php
+++ CodeHobby/resources/views/projects.blade.php
@@ -9,7 +9,12 @@
 		<h2>My Projects</h2>
 		These are some projects I put under my user (<a href='https://github.com/luigi1015'>luigi1015</a>) in GitHub.
 		<br>
-		<span class='note'>(Click on the titles to go to the GitHub page of the project.)</span>
+		They're in various states of working, in progress, and abandoned, and only some of which I will work on again.
+		<br>
+		So be aware of that if a project isn't working. If you submit a bug to a project, I may or may not ignore it.
+		<br>
+		<br>
+		<span class='note'>Click on the titles to go to the GitHub page of the project.</span>
 		<br>
 		@if( isset($projects) )
 			@foreach( $projects as $project )
